d7fc3f446cbfe866f0e8c614327b9a01afb88e1b
CIVIBL <brent@civentures.ca>
2026-09-23 11:04:45 -0400

    Add @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import { SITE_URL } from './src/lib/site.ts';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	site: SITE_URL,
+  output: 'static',
+  site: SITE_URL,
+  integrations: [sitemap()],
 });
